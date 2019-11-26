fixing normalization bug

transforms/im_transforms.py
@@ -3,8 +3,10 @@ Defining image transforms for data
 '''
 import torchvision.transforms as torchtransforms
 
+import numpy as np
 
-def get_fundamental_transforms(im_size, mean_val=0, std_val=1):
+
+def get_fundamental_transforms(im_size, mean_val=0.0, std_val=1.0):
   '''
   Performs the most basic transforms on input images
   '''
@@ -12,5 +14,6 @@ def get_fundamental_transforms(im_size, mean_val=0, std_val=1):
   return torchtransforms.Compose([
       torchtransforms.RandomCrop(size=im_size),
       torchtransforms.ToTensor(),
-      torchtransforms.Normalize(mean=mean_val, std=std_val)
+      torchtransforms.Normalize(mean=np.array(
+          [mean_val]), std=np.array([std_val]))
   ])