0a032bdfeb6a3866c3f3f396b21f9cae4d547b56
Ayush Baid <ayushrakeshbaid@gmail.com>
2019-11-26 17:56:54 -0500

    Reversing the modulus

--- runner/dataloader.py
+++ runner/dataloader.py
@@ -29,7 +29,7 @@ class TwoClassLoader(data.Dataset):
 
     return (
         self.transforms(Image.open(
-            self.files_classA[len(self.files_classA) % (idx+1)])),
+            self.files_classA[idx % len(self.files_classA)])),
         self.transforms(Image.open(random.choice(self.files_classB)))
     )
 
